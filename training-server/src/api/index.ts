f5183bb82df9a9714353e264ba97348de8cf55d3
aihki <laine.nestori.julius@gmail.com>
2024-03-19 19:11:06 +0200

    Add user route to API

--- training-server/src/api/index.ts
+++ training-server/src/api/index.ts
@@ -3,6 +3,7 @@ import foodDiaryRoute from './routes/foodDiaryRoute';
 import workoutRoute from './routes/workoutRoute';
 import excerciseRoute from './routes/excerciseRoute';
 import progressRoute from './routes/progressRoute';
+import userRoute from './routes/userRoute';
 
 
 const router = express.Router();
@@ -18,6 +19,7 @@ router.use('/food-diaries', foodDiaryRoute);
 router.use('/workouts', workoutRoute);
 router.use('/excercises', excerciseRoute);
 router.use('/progress-records', progressRoute);
+router.use('/users', userRoute);
 
 
 export default router;
