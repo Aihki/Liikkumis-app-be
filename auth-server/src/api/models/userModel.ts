9cc2451a3e251fb47c730ea669c48ad4b975f095
TomiHenriksson8 <tomihen01@gmail.com>
2024-04-12 18:24:52 +0300

    modify test setup

--- auth-server/src/api/models/userModel.ts
+++ auth-server/src/api/models/userModel.ts
@@ -221,6 +221,36 @@ const deleteUser = async (id: number): Promise<UserDeleteResponse | null> => {
   }
 };
 
+const postProfilePic = async (userId: number, user_profile_pic: string) => {
+  try {
+      const [rows] = await promisePool.execute<RowDataPacket[] & User[]>(
+          `
+          UPDATE Users
+          SET user_profile_pic = ?
+          WHERE user_id = ?`, [user_profile_pic, userId]
+      );
+      console.log('rows', rows)
+  } catch (e) {
+      console.error('error', (e as Error).message);
+      throw new Error((e as Error).message);
+  }
+};
+
+const fetchProfilePic = async (pic: string) => {
+  try {
+      const [rows] = await promisePool.execute<RowDataPacket[] & User[]>(
+          `
+          SELECT user_profile_pic
+          FROM Users
+          WHERE user_profile_pic= ?`, [pic]
+      );
+      return rows[0].user_profile_pic;
+  } catch (e) {
+      console.error('error', (e as Error).message);
+      throw new Error((e as Error).message);
+  }
+}
+
 export {
   getUserById,
   getAllUsers,
@@ -229,4 +259,6 @@ export {
   createUser,
   modifyUser,
   deleteUser,
+  postProfilePic,
+  fetchProfilePic
 };
